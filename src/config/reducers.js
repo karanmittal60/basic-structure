800a3c759ce59a45c3d67f721a4dae0f400d287a
unknown <ravibasil.empirecitylabs.com>
2019-09-06 19:26:51 +0530

    redux

--- src/config/reducers.js
+++ src/config/reducers.js
@@ -1,6 +1,22 @@
 import {combineReducers} from "redux";
 
 
+
+{
+    visibilityFilter: 'SHOW_ALL',
+        todos: [
+        {
+            text: 'Consider using Redux',
+            completed: true
+        },
+        {
+            text: 'Keep all state in a single tree',
+            completed: false
+        }
+    ]
+}
+
+
 const reducers =  combineReducers({
 
 });
