b4ba58ee42461999e054b5666d1cdff412853ccb
unknown <ravibasil.empirecitylabs.com>
2019-09-13 11:07:21 +0530

    pagination and utilities

---
+++ src/utilities/utilities.js
@@ -0,0 +1,13 @@
+const alphaOnly = event => {
+    if (!/^[a-z A-Z]*$/.test(event.key)) {
+        // alert("Please enter alphabet only");
+
+        event.preventDefault();
+    }
+};
+
+const alphaNumOnly = event => {
+    if (/[^0-9a-zA-Z]/.test(event.key)) {
+        event.preventDefault();
+    }
+};
